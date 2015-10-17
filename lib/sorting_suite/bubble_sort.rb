1e6e29a9f2a78e40ea3a596d8d7c1e6a587beb0c
Hector Huertas <hectorhuertas@Hectors-MacBook-Pro.local>
2015-10-17 10:16:47 -0600

    Implement BubbleSort class

---
+++ lib/sorting_suite/bubble_sort.rb
@@ -0,0 +1,23 @@
+module SortingSuite
+  class BubbleSort
+    def sort(array)
+      swap = true
+      while swap
+        swap = false
+
+        array.size.times do |pos|
+          if array[pos + 1] && array[pos + 1] < array[pos]
+            aux = array[pos + 1]
+            array[pos + 1] = array[pos]
+            array[pos] = aux
+            swap = true
+          end
+        end
+      end
+      array
+    end
+  end
+end
+
+sorter = SortingSuite::BubbleSort.new
+sorter.sort([4, 2, 5, 3, 1])
---
+++ test/sorting_suite/bubble_sort_test.rb
@@ -0,0 +1,39 @@
+require 'minitest'
+require 'sorting_suite/bubble_sort'
+
+class BubbleSortTest < Minitest::Test
+  def test_it_exists
+    sorter=SortingSuite::BubbleSort.new
+    assert sorter
+  end
+
+  def test_it_sorts_empty
+    sorter=SortingSuite::BubbleSort.new
+    assert_equal [],sorter.sort([])
+  end
+
+  def test_it_sorts_single
+    sorter=SortingSuite::BubbleSort.new
+    assert_equal [3],sorter.sort([3])
+  end
+
+  def test_it_sorts_even_of_fixnum
+    sorter=SortingSuite::BubbleSort.new
+    assert_equal [1,2,6,8],sorter.sort([6,2,8,1])
+  end
+
+  def test_it_sorts_even_of_string
+    sorter=SortingSuite::BubbleSort.new
+    assert_equal %w(c d e i o q),sorter.sort(%w(e c o d q i))
+  end
+
+  def test_it_sorts_odd_of_fixnum
+    sorter=SortingSuite::BubbleSort.new
+    assert_equal [1,2,6,8,9],sorter.sort([6,2,9,8,1])
+  end
+
+  def test_it_sorts_odd_of_string
+    sorter=SortingSuite::BubbleSort.new
+    assert_equal %w(c d e i o q x),sorter.sort(%w(e x c o d q i))
+  end
+end
