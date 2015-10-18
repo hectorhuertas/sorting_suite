bb7f39a1152f74352064f6fe414607cccb32f0e7
Hector Huertas <hectorhuertas@gmail.com>
2015-10-17 23:17:09 -0600

    Implement inplace_sort object_id test

--- lib/sorting_suite/insertion_sort.rb
+++ lib/sorting_suite/insertion_sort.rb
@@ -1,66 +1,57 @@
 module SortingSuite
   class InsertionSort
-    def sort(array_in)
-      ordered_array = []                                 # => []
-      array_in.each do |number|                          # => [4, 2, 6, 1]
-        inserted = false                                 # => false, false, false, false
-        place = nil                                      # => nil, nil, nil, nil
-        ordered_array.each do |ordered_number|           # => [], [4], [2, 4], [2, 4, 6]
-          if number < ordered_number                     # => true, false, false, true
-            inserted = true                              # => true, true
-            place = ordered_array.index(ordered_number)  # => 0, 0
+    def initialize(array=nil)
+      @array=array
+    end
+    def sort(array_in=nil)
+      if array_in.nil?
+        @array||=[]
+      else
+        @array=array_in
+      end
+      ordered_array = []
+      @array.each do |number|
+        inserted = false
+        place = nil
+        ordered_array.each do |ordered_number|
+          if number < ordered_number
+            inserted = true
+            place = ordered_array.index(ordered_number)
             break
           end
-        end                                              # => [], nil, [2, 4], nil
-        ordered_array.insert(place, number) if place     # => nil, [2, 4], nil, [1, 2, 4, 6]
+        end
+        ordered_array.insert(place, number) if place
 
-        ordered_array << number unless inserted  # => [4], nil, [2, 4, 6], nil
-      end                                        # => [4, 2, 6, 1]
-      ordered_array                              # => [1, 2, 4, 6]
+        ordered_array << number unless inserted
+      end
+      ordered_array
     end
 
-    def inplace_sort(array)
-      # array.size.times do |pos|
-      #   base=array[pos]
-      #   (array.size()-(pos-1))
-      #   (array.size()-(pos-1)).times do |pos2|
-      #     array[pos]
-      #     array[(pos2+1)]
-      #     if array[pos]<array[(pos2+1)]
-      #       puts 'swap'
-      #     end
-      #   end
-      1.upto(array.size()-1) do |count|                  # => 1
-        count                                            # => 1, 2, 3
-        count.times do |pos|                             # => 1, 2, 3
-          if array[count] < array[pos]                   # => true, false, false, true, false, false
+    def inplace_sort(array=nil)
+      if array.nil?
+        @array||=[]
+      else
+        @array=array
+      end
+      1.upto(@array.size()-1) do |count|
+
+        count.times do |pos|
+          if @array[count] < @array[pos]
             # array.delete_at(count)
-            array.insert(pos,array.delete_at(count)   )  # => [2, 5, 6, 1], [1, 2, 5, 6]
-          end                                            # => [2, 5, 6, 1], nil, nil, [1, 2, 5, 6], nil, nil
+            @array.insert(pos,@array.delete_at(count)   )
+          end
 
-        end  # => 1, 2, 3
+        end
 
-      end  # => 1
-      #
-      #
-      #
-      #
-      # end
+      end
 
-
-      array  # => [1, 2, 5, 6]
+      @array
     end
   end
 end
 
-sorter = SortingSuite::InsertionSort.new                                         # => #<SortingSuite::InsertionSort:0x007fbce20cb380>
-sorter.sort([4, 2, 6, 1])                                                        # => [1, 2, 4, 6]
-sorter.inplace_sort([5, 2, 6, 1])                                                # => [1, 2, 5, 6]
-# SortingSuite::InsertionSort.new([5, 2, 6, 1]).sort.object_id == array.object_id  # ~> ArgumentError: wrong number of arguments (1 for 0)
-
-# ~> ArgumentError
-# ~> wrong number of arguments (1 for 0)
-# ~>
-# ~> /Users/hectorhuertas/turing/1module/projects/sorting_suite/lib/sorting_suite/insertion_sort.rb:59:in `initialize'
-# ~> /Users/hectorhuertas/turing/1module/projects/sorting_suite/lib/sorting_suite/insertion_sort.rb:59:in `new'
-# ~> /Users/hectorhuertas/turing/1module/projects/sorting_suite/lib/sorting_suite/insertion_sort.rb:59:in `<main>'
+sorter = SortingSuite::InsertionSort.new
+sorter.sort([4, 2, 6, 1])
+sorter.inplace_sort([5, 2, 6, 1])
+array=[5, 2, 6, 1]
+SortingSuite::InsertionSort.new(array).inplace_sort.object_id == array.object_id
